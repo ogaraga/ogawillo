6fbf0738f3648513c2fc2d4e381ea4e40b48cd5e
Ogaraga <raymond.ogaraga@gmail.com>
2023-11-13 20:28:14 +0100

    review

--- DONATE HERE/java.js
+++ DONATE HERE/java.js
@@ -1,44 +1,38 @@
-const oneTime = document.getElementById("btn");
-const monTime = document.getElementById("btn-mon");
+
+const oneTime = document.getElementById("btn_one");
+const monTime = document.getElementById("btn_mon");
 const oneTimMon = document.getElementById("one_time");
 const monTimMon = document.getElementById("_monthly");
-const oneTimCur = document.getElementById("symbol-1");
-const monTimCur = document.getElementById("symbol-2");
+
+const sim_1 = document.getElementById("sim-1");
+const sim_2 = document.getElementById("sim-2");
 
 monTime.addEventListener("click",()=>{
-    if (monTimMon.style.display === "none" && monTimCur.style.display === "none") {
-       monTimMon.style.display = "block";
-       monTimCur.style.display = "block";
-       oneTimMon.style.display = "none";
-       oneTimCur.style.display = "none"; 
+    if (monTimMon.style.display === "none") {
+       monTimMon.style.display = "flex";
+       oneTimMon.style.display = "none"; 
+    }
+    else{
+        monTimMon.style.display = "flex"; 
     }
     
 } )
 
 oneTime.addEventListener("click",()=>{
-    if (monTimMon.style.display === "block" && monTimCur.style.display === "block") {
+    if (monTimMon.style.display === "flex") {
+       oneTimMon.style.display = "flex"; 
        monTimMon.style.display = "none";
-       monTimCur.style.display = "none";
-       oneTimMon.style.display = "block";
-       oneTimCur.style.display = "block"; 
     }
     else{
-        oneTimMon.style.display = "block";
-        oneTimCur.style.display = "block";
-        monTimMon.style.display = "none";
-        monTimCur.style.display = "none";
+       oneTimMon.style.display = "flex";
         
     }    
 } )
 
-
-var one_Times = document.getElementsByClassName("one_time");
 var once = document.getElementById("one-time");
 var monthly = document.getElementById("monthly");
-var total = document.getElementById("total_1");
-var total_2 = document.getElementById("total");
-var symbol_1 = document.getElementById("symbol-1");
-var symbol_2 = document.getElementById("symbol-2");
+var total_1 = document.getElementById("total_1");
+var total_2 = document.getElementById("total_2");
 var naira = document.getElementById("naira");
 var dollar = document.getElementById("dollar");
 var pounds = document.getElementById("pounds");
@@ -48,30 +42,30 @@ var dollar2 = document.getElementById("dollar_2");
 var pounds2 = document.getElementById("pounds_2");
 var euro2 = document.getElementById("euro_2");
 function Fmoney(Fvalue){
-    total.value = "";
-    total.value += `₦${Fvalue}`;
+    total_1.value = "";
+    total_1.value += `₦${Fvalue}`;
     
-    if(symbol_1.value === "USD"){
-        total.value = "";
+    if(sim_1.value === "USD"){
+        total_1.value = "";
         dollar.value = "USD"
-        total.value += `$${Fvalue}`;
+        total_1.value += `$${Fvalue}`;
     }
     
-    else if(symbol_1.value === "GBP"){
-        total.value = "";
+    else if(sim_1.value === "GBP"){
+        total_1.value = "";
         pounds.value = "GBP";
-        total.value += `£${Fvalue}`;
+        total_1.value += `£${Fvalue}`;
 
     }
 
-    else if(symbol_1.value === "EUR"){
-        total.value = "";
+    else if(sim_1.value === "EUR"){
+        total_1.value = "";
         euro.value = "EUR";
-        total.value += `€${Fvalue}`;
+        total_1.value += `€${Fvalue}`;
 
     }
     else{
-        total.value = `₦${Fvalue}`;
+        total_1.value = `₦${Fvalue}`;
         naira.value = "NGN";
     }
 }
@@ -79,43 +73,42 @@ function Fcash(fvalue) {
     total_2.value = "";
     total_2.value += `₦${fvalue}`;
     
-    if(symbol_2.value === "USD"){
+    if(sim_2.value === "USD"){
         total_2.value = "";
         dollar2.value = "USD"
         total_2.value += `$${fvalue}`;
     }
     
-    else if(symbol_2.value === "GBP"){
+    else if(sim_2.value === "GBP"){
         total_2.value = "";
         pounds2.value = "GBP";
         total_2.value += `£${fvalue}`;
 
     }
 
-    else if(symbol_2.value === "EUR"){
+    else if(sim_2.value === "EUR"){
         total_2.value = "";
         euro2.value = "EUR";
         total_2.value += `€${fvalue}`;
 
     }
     else{
-        total.value = `₦${fvalue}`;
-        naira.value = "NGN";
+        total_2.value = `₦${fvalue}`;
+        naira2.value = "NGN";
     }
 
 }
 const next = document.getElementById("next");
 const validation = document.getElementById("validated");
 next.onclick = ()=>{
-    if(total.value !== "" && total_2 !== ""){
-        total.value = "";
-        total_2.value = "";
+    
+    if(total_1.value !== "" || total_2.value !== ""){
         validation.innerText = "Selection saved successfully";
         validation.style.color = "darkgreen";
+        total_1.value = "";
+        total_2.value = "";
     }
     else{
-        total.value = "";
-        total_2.value = "";
         validation.innerText = "Try make selection if you can";
         validation.style.color = "red";
     }
@@ -123,11 +116,11 @@ next.onclick = ()=>{
 }
 oneTime.onclick = ()=>{
     validation.innerText = "";
-    total.value = "";
+    total_1.value = "";
     total_2.value = "";
 }
 monTime.onclick = ()=>{
     validation.innerText = "";
-    total.value = "";
+    total_1.value = "";
     total_2.value = "";
 }
